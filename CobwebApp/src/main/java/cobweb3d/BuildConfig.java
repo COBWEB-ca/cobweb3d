36197072953af0424c209eea1609d16a09aea02e
Adam <adam@adli.ca>
2018-05-18 16:46:56 -0400

    Version 0.1.4 tagged

--- CobwebApp/src/main/java/cobweb3d/BuildConfig.java
+++ CobwebApp/src/main/java/cobweb3d/BuildConfig.java
@@ -1,6 +1,6 @@
 package cobweb3d;
 
 public class BuildConfig {
-    public static String VERSION = "0.1.4";
+    public static String VERSION = "0.1.4a";
     public static boolean DEBUG = false;
 }
