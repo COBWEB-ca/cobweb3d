632c02a7101aaad1ffc062f33973b9f402144f92
Zewen <41880356@qq.com>
2018-06-24 13:23:53 +0800

    Finish the document for package cobweb3d.core.entity

--- CobwebApp/src/main/java/cobweb3d/core/entity/Cause.java
+++ CobwebApp/src/main/java/cobweb3d/core/entity/Cause.java
@@ -1,5 +1,14 @@
 package cobweb3d.core.entity;
 
+/**
+ * This interface intends to illustrate the reason why an agent's energy is changed.
+ * However, it seems that this interface hasn't been used in current version (0.1.4)
+ * Anyway I think this interface is useful, since it enables researchers to figure out
+ * why an agent dies.
+ *
+ * Document Author: Zewen Shen
+ * Reminder: I'm not the author of this class, so my document may be inaccurate.
+ */
 public interface Cause {
     String getName();
 }
