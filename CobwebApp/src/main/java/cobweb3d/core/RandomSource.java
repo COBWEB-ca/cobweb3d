bddf5deacbcd91ee33fa061d97538190eb86db4d
Zewen <41880356@qq.com>
2018-06-24 14:02:49 +0800

    Finish the document for package cobweb3d.core

--- CobwebApp/src/main/java/cobweb3d/core/RandomSource.java
+++ CobwebApp/src/main/java/cobweb3d/core/RandomSource.java
@@ -2,6 +2,12 @@ package cobweb3d.core;
 
 import cobwebutil.RandomNoGenerator;
 
+/**
+ * Classes implementing this should be able to return a random number generator.
+ *
+ * Document Author: Zewen Shen
+ * Reminder: I'm not the author of this class, so my document may be inaccurate.
+ */
 public interface RandomSource {
 
     RandomNoGenerator getRandom();
--- CobwebApp/src/main/java/cobweb3d/core/SimulationTimeSpace.java
+++ CobwebApp/src/main/java/cobweb3d/core/SimulationTimeSpace.java
@@ -2,6 +2,14 @@ package cobweb3d.core;
 
 import cobweb3d.core.environment.Topology;
 
+/**
+ * This interface is a sub-interface of interface RandomSource, which is a random generator interface.
+ * So generally any class implementing this interface must be able to return a time, the topology of the environment,
+ * and able to generate random number. (It's a little bit confusing for me :( )
+ *
+ * Document Author: Zewen Shen
+ * Reminder: I'm not the author of this class, so my document may be inaccurate.
+ */
 public interface SimulationTimeSpace extends RandomSource {
     long getTime();
 
