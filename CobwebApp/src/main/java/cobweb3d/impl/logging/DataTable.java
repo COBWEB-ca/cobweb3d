f3c6e599bbd4f04784965b257fc2862b686e7441
Zewen <41880356@qq.com>
2018-06-26 18:05:06 +0800

    Documentation for cobweb3d/impl/logging/DataTable.java

--- CobwebApp/src/main/java/cobweb3d/impl/logging/DataTable.java
+++ CobwebApp/src/main/java/cobweb3d/impl/logging/DataTable.java
@@ -4,8 +4,13 @@ import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
+/**
+ * This is a 2D table.
+ */
 public class DataTable {
+    // Given a integer, rowMap returns the row whose index is the integer.
     public SortedMap<Integer, SmartLogRow> rowMap;
+    // columnInts represents the column names of the DataTable.
     public SortedMap<Integer, String> columnInts;
 
     public DataTable(String... columnNames) {
@@ -14,6 +19,11 @@ public class DataTable {
         addColumns(columnNames);
     }
 
+    /**
+     * Add more columns to the DataTable
+     *
+     * @param columnNames the names of columns which are going to be added
+     */
     public void addColumns(String... columnNames) {
         int prevSize = columnInts.size();
         for (int i = 0; i < columnNames.length; i++) {
@@ -21,10 +31,22 @@ public class DataTable {
         }
     }
 
+    /**
+     * Add one more column to the DataTable
+     *
+     * @param columnName the name of the column which is going to be added
+     */
     public void addColumn(String columnName) {
         if (!columnInts.containsValue(columnName)) this.columnInts.put(columnInts.size(), columnName);
     }
 
+    /**
+     * Return the selected row of the DataTable.
+     * If the row doesn't exist, it will initialize an empty row based on that index and return it.
+     *
+     * @param tick the index of row which is selected.
+     * @return the row corresponding to the index.
+     */
     public SmartLogRow getRow(int tick) {
         if (rowMap.containsKey(tick)) return rowMap.get(tick);
         else {
@@ -34,6 +56,9 @@ public class DataTable {
         }
     }
 
+    /**
+     * This class represents a row of the log table.
+     */
     public class SmartLogRow {
         public Map<String, Integer> columns;
         public Map<Integer, Cell> cells = new TreeMap<>();
@@ -50,6 +75,13 @@ public class DataTable {
             }
         }
 
+        /**
+         * It lets the content of the cell selected by columnIndex to be the given value.
+         * Note: If the cell selected by columnIndex has already mapped to a value, that value will be overwritten.
+         *
+         * @param columnIndex the index of the selected cell
+         * @param value the value which will be put into that cell
+         */
         public void putVal(int columnIndex, Object value) {
             if (!cells.containsKey(columnIndex)) cells.put(columnIndex, new Cell(value));
             else {
@@ -58,6 +90,9 @@ public class DataTable {
             }
         }
 
+        /**
+         * A container which stores one object.
+         */
         public class Cell {
             public Object value;
 
