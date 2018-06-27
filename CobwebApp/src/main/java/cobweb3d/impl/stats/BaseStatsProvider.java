45a0b80c5d5675da3b1cb186569c6c387218b3bd
Zewen <41880356@qq.com>
2018-06-27 10:29:48 +0800

    Finish the documentation for package cobweb3d.impl.stats

--- CobwebApp/src/main/java/cobweb3d/impl/stats/BaseStatsProvider.java
+++ CobwebApp/src/main/java/cobweb3d/impl/stats/BaseStatsProvider.java
@@ -7,6 +7,9 @@ import cobweb3d.plugins.mutators.DataLoggingMutator;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * This class collects data produced from a simulation.
+ */
 public class BaseStatsProvider {
     protected Simulation simulation;
 
@@ -14,6 +17,11 @@ public class BaseStatsProvider {
         this.simulation = simulation;
     }
 
+    /**
+     * Count agents' total energy in current tick of the simulation.
+     *
+     * @return the total energy of all the current agents
+     */
     public long countAgentEnergy() {
         long totalEnergy = 0;
         for (BaseAgent a : simulation.environment.getAgents()) totalEnergy += a.getEnergy();
@@ -24,12 +32,24 @@ public class BaseStatsProvider {
         return simulation.environment.getAgentCount();
     }
 
+    /**
+     * Count the number of agents which belong to the given type.
+     *
+     * @param agentType the type of agents that one is going to count
+     * @return the number of agents which belong to the given type
+     */
     public long countAgents(int agentType) {
         long count = 0;
         for (BaseAgent a : simulation.environment.getAgents()) if (a.getType() == agentType) count++;
         return count;
     }
 
+    /**
+     * Count the total energy of agents which belong to the given type.
+     *
+     * @param agentType the type of agents that one is going to count
+     * @return the total energy of agents which belong to the given type
+     */
     public long countAgentEnergy(int agentType) {
         long totalEnergy = 0;
         for (BaseAgent a : simulation.environment.getAgents())
