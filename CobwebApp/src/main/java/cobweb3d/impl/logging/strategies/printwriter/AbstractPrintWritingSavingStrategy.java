203a9d45f446ddd55ec9fa2d81401c0b8abf518f
Zewen <41880356@qq.com>
2018-06-29 17:04:18 +0800

    Finish the documentations for package cobweb3d.impl.logging.strategies.printwriter

--- CobwebApp/src/main/java/cobweb3d/impl/logging/strategies/printwriter/AbstractPrintWritingSavingStrategy.java
+++ CobwebApp/src/main/java/cobweb3d/impl/logging/strategies/printwriter/AbstractPrintWritingSavingStrategy.java
@@ -14,13 +14,25 @@ public abstract class AbstractPrintWritingSavingStrategy implements SavingStrate
     private File lastFile;
     private int lastSavedTick = 0;
 
+    /**
+     * A wrapper method for saveDataToPrintWriter().
+     * This method determines whether or not the program is writing into a new file,
+     * and opens IO stream, which is passed into saveDataToPrintWriter().
+     *
+     * @param coreData An instance of class DataTable which comprises data of a simulation.
+     * @param plugins A collection of plugins which provide the name of each column.
+     * @param file The file where data would be written in.
+     * @return The last tick of data that has already been saved.
+     */
     @Override
     public int save(DataTable coreData, Collection<DataLoggingMutator> plugins, File file) {
         if (file == null) return 0;
+        // If the last file doesn't exist or is different from current file, we will rewrite coreData into the new file.
         if (lastFile == null || (!lastFile.equals(file) && !lastFile.getAbsolutePath().equals(file.getAbsolutePath())))
             lastSavedTick = 0;
         try {
             PrintWriter printWriter = new PrintWriter(new FileOutputStream(file, true));
+            // plugins are used to provide column names only if we are writing into a new file.
             this.lastSavedTick = saveDataToPrintWriter(printWriter, coreData, plugins, this.lastSavedTick);
             printWriter.flush();
             printWriter.close();
--- CobwebApp/src/main/java/cobweb3d/impl/logging/strategies/printwriter/PlainTextSavingStategy.java
+++ CobwebApp/src/main/java/cobweb3d/impl/logging/strategies/printwriter/PlainTextSavingStategy.java
@@ -1,5 +1,8 @@
 package cobweb3d.impl.logging.strategies.printwriter;
 
+/**
+ * This class use tab to split items in a same line.
+ */
 public class PlainTextSavingStategy extends CSVSavingStrategy {
     public PlainTextSavingStategy() {
         super('\t');
